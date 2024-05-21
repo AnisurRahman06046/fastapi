27d578391a8b84768d85ac8fb124fadcb1538775
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-21 15:04:58 +0600

    multiple path and query parameters

--- Day1/main.py
+++ Day1/main.py
@@ -6,6 +6,9 @@ class ModelName(str,Enum):
     alexnet='alexnet'
     resnet = 'resnet'
     lenet = 'lenet'
+    
+    
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/")
 async def root():
@@ -13,7 +16,9 @@ async def root():
 
 # path parameters
 @app.get("/items/{id}")
-async def getItems(id:int):
+async def getItems(id:int,q:str | None = None):
+    if q:
+        return {"itemid":id,"q":q}
     return {"itemid":id}
 
 
@@ -29,3 +34,21 @@ async def getModels(model_name:ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path:str):
     return {"file_path":file_path}
+
+
+# query parameters
+@app.get("/items")
+async def readItems(skip:int=0,limit:int=10):
+    return fake_items_db[skip:skip+limit]
+
+
+# multiple path and query parameters
+@app.get("/users/{userid}/items/{itemid}")
+async def read_user_item(userid:int,itemid:str,q:str | None = None, short:bool=False):
+    item = {"item_id": userid, "owner_id": itemid}
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update({"description":"this is  an amazing item"})
+        
+    return item
